55b1f7a8bf9cc912486f62fafef2e6f12a3824a8
Dane Palazzo <PalazzoDD@comcast.net>
2023-11-21 14:14:03 -0700

    completed deliverables

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Research, Author, ResearchAuthors
+from models import db , Research, Author, ResearchAuthors
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -13,6 +13,54 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
+
+api = Api(app)
+
+class ResearchFetch(Resource):
+    def get(self):
+        research = [research.to_dict(rules=("-research_authors",)) for research in Research.query.all()]
+        return research, 200
+
+class ResearchByID(Resource):
+    def get(self, id):
+        research = Research.query.filter_by(id=id).first()
+        if not research:
+            return {"error": "Research paper not found"}, 404
+        return research.to_dict(), 200
+    
+    def delete(self, id):
+        research = Research.query.filter_by(id=id).first()
+        if not research:
+            return {"error": "Research paper not found"}, 404
+        db.session.delete(research)
+        db.session.commit()
+        return {}, 204
+
+class Authors(Resource):
+    def get(self):
+        authors = [author.to_dict(rules=("-research_authors",)) for author in Author.query.all()]
+        return authors, 200
+    
+
+class ResaerchAuthor(Resource):
+    def post(self):
+        try:
+            new_research_author = ResearchAuthors(
+                author_id = request.json["author_id"],
+                research_id = request.json["research_id"],
+            )
+            db.session.add(new_research_author)
+            db.session.commit()
+            return new_research_author.to_dict(), 201
+        except:
+            return {"errors": ["validation errors"]}, 400
+
+
+
+api.add_resource(ResearchFetch, "/research")
+api.add_resource(ResearchByID, "/research/<int:id>")
+api.add_resource(Authors, "/authors")
+api.add_resource(ResaerchAuthor, "/research_author")
 
 @app.route('/')
 def index():
--- server/models.py
+++ server/models.py
@@ -11,3 +11,48 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+
+class Research(db.Model, SerializerMixin):
+    __tablename__ = "research_table"
+    id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String)
+    year = db.Column(db.Integer)
+    page_count = db.Column(db.Integer)
+    research_authors = db.relationship("ResearchAuthors", back_populates="research")
+
+    serialize_rules=("-research_authors.research",)
+
+    @validates("year")
+    def validates_year(year):
+        if not year or len(year) is not 4:
+            raise ValueError("Year must be a valid 4 diget year")
+        return year
+        
+
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = "author_table"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    field_of_study = db.Column(db.String)
+    research_authors = db.relationship("ResearchAuthors", back_populates="author")
+    
+    serialize_rules=("-research_authors.author",)
+
+    @validates("field_of_study")
+    def validates_field_of_study(field_of_study):
+        valid_list = ["AI", "ROBOTICS", "MACHINE LEARNING", "VISION", "CYBERSECURITY"]
+        if field_of_study.upper() not in valid_list:
+            raise ValueError('Filed of Study must be "AI", "Robotics", "Machine Learning", "Vision", "Cybersecurity"')
+        return field_of_study
+
+
+class ResearchAuthors(db.Model, SerializerMixin):
+    __tablename__ = "research_authors_table"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey("author_table.id"))
+    author = db.relationship("Author", back_populates="research_authors")
+    research_id = db.Column(db.Integer, db.ForeignKey("research_table.id"))
+    research = db.relationship("Research", back_populates="research_authors")
+
+    serialize_rules=("-author.research_authors", "-research.research_authors")
